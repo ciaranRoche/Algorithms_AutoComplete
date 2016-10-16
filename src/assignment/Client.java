eb7cd4dc88ed00ae0a3e317d4a721d18b3278ecc
ciaran <ciaranroche@hotmail.co.uk>
2016-10-16 11:01:14 +0100

    Client method basic added

--- src/assignment/Client.java
+++ src/assignment/Client.java
@@ -1,5 +1,65 @@
 package assignment;
 
+import java.util.Scanner;
+
 public class Client {
+	
+	private static String prefix;
+	private static int k;
+
+	
+	public Client(){
+	}
+	
+	public static void main(String[] args){
+		run();
+		System.out.println("k: " + k + ", prefix: "+prefix);
+	}
+	
+	public static void run(){
+		Scanner scanner = new Scanner(System.in);
+		try{
+		System.out.println("Please input a prefix in which you want to search for matchs:");
+		String inputP = scanner.nextLine();
+	    setPrefix(inputP);
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+			run();
+		}
+		try{
+		System.out.println("Please enter a max search number between 1-10:");
+		int inputK = scanner.nextInt();
+		if(inputK > 10){
+			System.out.println("Invalid option, please pick a number between 1 and 10:");
+			run();
+		}else{
+		setK(inputK);
+		}
+		}catch(Exception i){
+			System.out.println("Invalid option: " + i.getMessage());
+			run();
+		}
+	}
+	
+	
+	public String toString(){
+		return "Prefix:" + prefix + ", Number of wanted matches: " + getK();
+	}
+	
+	public static void setPrefix(String prefix){
+		Client.prefix = prefix;
+	}
+	
+	public String prefix(){
+		return prefix;
+	}
+
+	public static int getK() {
+		return k;
+	}
+
+	public static void setK(int k) {
+		Client.k = k;
+	}
 
 }
